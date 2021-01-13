50f5cff74f3ad6a3f82cbaf6778c89e82178a425
Malo Pivert <pivertmalo@gmail.com>
2021-01-13 15:01:02 +0100

    auth middleware commented and test.js route not working

--- src/app.js
+++ src/app.js
@@ -4,15 +4,13 @@ const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config();
 
-const { jwtDecode } = require('./auth/middlewares');
+// const { jwtDecode } = require('./auth/middlewares');
 
-const middlewares = require('./auth/middlewares');
+// const middlewares = require('./auth/middlewares');
 
 // routes
-// eslint-disable-next-line import/no-unresolved
+
 const routes = require('./routes');
-// eslint-disable-next-line import/no-unresolved
-const routesauth = require('./auth');
 
 const app = express();
 
@@ -21,12 +19,11 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use(jwtDecode);
+// app.use(jwtDecode);
 
 app.use('/api/v1', routes);
-app.use('/api/v1', routesauth);
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+// app.use(middlewares.notFound);
+// app.use(middlewares.errorHandler);
 
 module.exports = app;
