85834a9b67afa9083b088f9763fd7ad5cf8c337e
De Zaldua Samuel <samuel.dezaldua@gmail.com>
2021-01-13 22:08:41 +0100

    basic seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,63 +1,80 @@
 const prisma = require('../src/prismaClient');
 
 (async () => {
-  await prisma.city.create({});
-  // NAME
 
-  const nameTab = prisma.city.create({
-    city: 'Paris',
-    country: 'France',
-    text: 'fromage',
-    longitude: 10,
-    latitude: 111,
-    video_id: 1,
-    image_id: 1,
-  });
+  await prisma.user.create({
+    data: {
+      email: 'admin@dev.com',
+      password: 'superadmin'
+    }
+  })
+
+  await prisma.city.create({
+    data: {
+      name: 'Paris', 
+      country: 'France',
+      text: 'fromage',
+      longitude: 10,
+      latitude: 111
+    }
+  })
+  // await prisma.city.create({});
+  // // NAME
+
+  // const nameTab = prisma.city.create({
+  //   city: 'Paris',
+  //   country: 'France',
+  //   text: 'fromage',
+  //   longitude: 10,
+  //   latitude: 111,
+  //   video_id: 1,
+  //   image_id: 1,
+  // });
   // COUNTRY
 
-  const country = ['France', 'United Kingdom', 'Spain', 'Portugal', 'Germany'];
-  const countryTab = country.map((item) =>
-    prisma.city.create({ country: { label: item } })
-  );
+  // const country = ['France', 'United Kingdom', 'Spain', 'Portugal', 'Germany'];
+  // const countryTab = country.map((item) =>
+  //   prisma.city.create({ country: { label: item } })
+  // );
 
-  // TEXT
+  // // TEXT
 
-  const text = ['Fromage', 'Jelly', 'Paella', 'Sausage', 'Beer'];
-  const textTab = text.map((item) =>
-    prisma.city.create({ text: { label: item } })
-  );
+  // const text = ['Fromage', 'Jelly', 'Paella', 'Sausage', 'Beer'];
+  // const textTab = text.map((item) =>
+  //   prisma.city.create({ text: { label: item } })
+  // );
 
-  // LONGITUDE
+  // // LONGITUDE
 
-  const longitude = ['001', '002', '003', '004', '005'];
-  const longitudeTab = longitude.map((item) =>
-    prisma.city.create({ longitude: { label: item } })
-  );
+  // const longitude = ['001', '002', '003', '004', '005'];
+  // const longitudeTab = longitude.map((item) =>
+  //   prisma.city.create({ longitude: { label: item } })
+  // );
 
-  // LATITUDE
+  // // LATITUDE
 
-  const latitude = ['111', '222', '333', '444', '555'];
-  const latitudeTab = latitude.map((item) =>
-    prisma.city.create({ latitude: { label: item } })
-  );
+  // const latitude = ['111', '222', '333', '444', '555'];
+  // const latitudeTab = latitude.map((item) =>
+  //   prisma.city.create({ latitude: { label: item } })
+  // );
 
-  // VIDEO ID
+  // // VIDEO ID
 
-  const videoId = ['1', '2', '3', '4', '5'];
-  const videoTab = videoId.map((item) =>
-    prisma.city.create({ text: { label: item } })
-  );
+  // const videoId = ['1', '2', '3', '4', '5'];
+  // const videoTab = videoId.map((item) =>
+  //   prisma.city.create({ text: { label: item } })
+  // );
 
-  // IMAGE ID
+  // // IMAGE ID
 
-  const imageId = ['1', '2', '3', '4', '5'];
-  const imageTab = imageId.map((item) =>
-    prisma.city.create({ text: { label: item } })
-  );
+  // const imageId = ['1', '2', '3', '4', '5'];
+  // const imageTab = imageId.map((item) =>
+  //   prisma.city.create({ text: { label: item } })
+  // );
 
-  await Promise.all(nameTab).then(() => {
-    console.log('Seeds done !');
-  });
+  // await Promise.all(nameTab).then(() => {
+  //   console.log('Seeds done !');
+  // });
 })().finally(async () => {
   console.log('city seed done');
   await prisma.$disconnect();
