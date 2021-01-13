7c87fc048a9f5f5c8f21e4c02a80c74d92c3d755
Malo Pivert <pivertmalo@gmail.com>
2021-01-13 16:07:14 +0100

    creating the auth routes and db generated with prisma

--- src/app.js
+++ src/app.js
@@ -9,8 +9,10 @@ require('dotenv').config();
 // const middlewares = require('./auth/middlewares');
 
 // routes
-
+// eslint-disable-next-line import/no-unresolved
 const routes = require('./routes');
+
+const routeAuth = require('./auth');
 
 const app = express();
 
@@ -19,9 +21,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-// app.use(jwtDecode);
-
-app.use('/api/v1', routes);
+app.use('/', routes);
+app.use('/auth', routeAuth);
 
 // app.use(middlewares.notFound);
 // app.use(middlewares.errorHandler);
