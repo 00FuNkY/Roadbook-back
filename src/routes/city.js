865fac2e652a41c63925cde369035d86a1db65ec
yoanmarcos <yomarcos@free.fr>
2021-01-15 11:08:33 +0100

    upload first part DONE

--- src/routes/city.js
+++ src/routes/city.js
@@ -75,32 +75,36 @@ router.get('/:id', async (req, res, next) => {
 
 // POST
 
-router.post('/', uploads.array('files', 10), async (req, res, next) => {
-  try {
-    console.log(req.files);
-    req.files.forEach((file) => {
-      const splittedFilename = file.originalname.split('.');
-      const extension = splittedFilename[splittedFilename.length - 1];
-      fs.rename(
-        `${process.cwd()}/uploads/${file.filename}`,
-        `${process.cwd()}/uploads/${file.filename}.${extension}`,
-        async (err) => {
-          if (err) return next(err);
-          //   await db.image.create({
-          //     data: {
-          //       path: `/uploads/${file.filename}.${extension}`,
-          //     },
-          //   });
-        }
-      );
-    });
+router.post(
+  '/:id/upload',
+  uploads.array('files', 10),
+  async (req, res, next) => {
+    try {
+      console.log(req.files);
+      req.files.forEach((file) => {
+        const splittedFilename = file.originalname.split('.');
+        const extension = splittedFilename[splittedFilename.length - 1];
+        fs.rename(
+          `${process.cwd()}/uploads/${file.filename}`,
+          `${process.cwd()}/uploads/${file.filename}.${extension}`,
+          async (err) => {
+            if (err) return next(err);
+            //   await db.image.create({
+            //     data: {
+            //       path: `/uploads/${file.filename}.${extension}`,
+            //     },
+            //   });
+          }
+        );
+      });
 
-    return res.sendStatus(201);
-  } catch (error) {
-    console.log(error);
-    return next(error);
+      return res.sendStatus(201);
+    } catch (error) {
+      console.log(error);
+      return next(error);
+    }
   }
-});
+);
 
 // DELETE
 
