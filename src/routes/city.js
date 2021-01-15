c9fb2b0e8a7663a8cdaeddcabb6aa6775f4a8683
yoanmarcos <yomarcos@free.fr>
2021-01-15 10:43:50 +0100

    update the post route

--- src/routes/city.js
+++ src/routes/city.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const multer = require('multer');
-// const fs = require('fs');
+const fs = require('fs');
 // const cors = require('cors');
 const prisma = require('../prismaClient');
 
-const upload = multer({ dest: 'uploads/' });
+const uploads = multer({ dest: 'uploads/' });
 
 const router = express.Router();
-
-// router.use(cors());
 
 // GET
 
@@ -58,28 +56,50 @@ router.get('/:id', async (req, res, next) => {
 
 // POST
 
-router.post('/', async (req, res, next) => {
-  try {
-    const results = await prisma.city.create({
-      data: {
-        name: req.body.name,
-        country: req.body.country,
-        text: req.body.text,
-        longitude: req.body.longitude,
-        latitude: req.body.latitude,
-      },
-    });
-    res.status(201).json(results);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const results = await prisma.city.create({
+//       data: {
+//         name: req.body.name,
+//         country: req.body.country,
+//         text: req.body.text,
+//         longitude: req.body.longitude,
+//         latitude: req.body.latitude,
+//       },
+//     });
+//     res.status(201).json(results);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 // POST
 
-router.post('/:id/upload', upload.array('files', 10), (req, res) => {
-  console.log(req.files);
-  return res.sendStatus(200);
+router.post('/', uploads.array('files', 10), async (req, res, next) => {
+  try {
+    console.log(req.files);
+    req.files.forEach((file) => {
+      const splittedFilename = file.originalname.split('.');
+      const extension = splittedFilename[splittedFilename.length - 1];
+      fs.rename(
+        `${process.cwd()}/uploads/${file.filename}`,
+        `${process.cwd()}/uploads/${file.filename}.${extension}`,
+        async (err) => {
+          if (err) return next(err);
+          //   await db.image.create({
+          //     data: {
+          //       path: `/uploads/${file.filename}.${extension}`,
+          //     },
+          //   });
+        }
+      );
+    });
+
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
 });
 
 // DELETE
